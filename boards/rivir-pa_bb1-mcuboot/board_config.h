2f226589686beeead88085c22e393ef03731865d
Stephen Irons <stephen.irons@iotec.space>
2024-05-09 17:44:23 +1200

    New build for pa-bb1-mcuboot.

---
+++ boards/rivir-pa_bb1-mcuboot/board_config.h
@@ -0,0 +1,26 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define VENDOR_NAME "RIVIR"
+#define PRODUCT_NAME "bloader PetroAlpha_BB-1"
+#define VOLUME_LABEL "RIVIRBOOT"
+#define INDEX_URL "http://rivir.space"
+#define BOARD_ID "SAME51J20A-RIVIR-PA-BB1"
+
+#define USB_VID 0x239A
+#define USB_PID 0x00CD
+
+#define LED_PIN PIN_PA19
+
+#define BOOT_USART_MODULE                 SERCOM0
+#define BOOT_USART_MASK                   APBAMASK
+#define BOOT_USART_BUS_CLOCK_INDEX        MCLK_APBAMASK_SERCOM0
+#define BOOT_USART_PAD_SETTINGS           UART_RX_PAD3_TX_PAD0
+#define BOOT_USART_PAD3                   PINMUX_PA07D_SERCOM0_PAD3
+#define BOOT_USART_PAD2                   PINMUX_UNUSED
+#define BOOT_USART_PAD1                   PINMUX_UNUSED
+#define BOOT_USART_PAD0                   PINMUX_PA04D_SERCOM0_PAD0
+#define BOOT_GCLK_ID_CORE                 SERCOM0_GCLK_ID_CORE
+#define BOOT_GCLK_ID_SLOW                 SERCOM0_GCLK_ID_SLOW
+
+#endif
